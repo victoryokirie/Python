9a61c1b71d1211fb798edf7720fd8bbc31de73e4
victory okirie <victoryokirie98@gmail.com>
2024-05-12 13:37:55 +0100

    snake collides with food, food location changes

--- snake_game/main.py
+++ snake_game/main.py
@@ -27,6 +27,10 @@ while game_is_on:
     time.sleep(0.1)
     
     snake.move()
+
+    ## Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
         
         
 

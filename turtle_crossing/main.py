4f896c0338fa6e10a7fc712193574c8171542338
victory okirie <victoryokirie98@gmail.com>
2024-05-18 18:15:06 +0100

    created car manager

--- turtle_crossing/main.py
+++ turtle_crossing/main.py
@@ -1,6 +1,7 @@
 import time
 from turtle import Screen
 from player import Player
+from car_manager import CarManager
 
 
 screen = Screen()
@@ -8,6 +9,7 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
 
 screen.listen()
 screen.onkey(player.move_up, "Up")
@@ -18,6 +20,8 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
 
+    car_manager.create_car()
+    car_manager.move_cars()
 
 
 
